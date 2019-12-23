6564c477292e81a96857fc4f809a8d019a49d27e
昝欢 <2985459100@qq.com>
2019-12-23 15:01:11 +0800

    firsts

---
+++ src/main/java/com/zanhaun/springboot/SpringBootConfigAuto2Application.java
@@ -0,0 +1,13 @@
+package com.zanhaun.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigAuto2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigAuto2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zanhaun/springboot/SpringBootConfigAuto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zanhaun.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigAuto2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
